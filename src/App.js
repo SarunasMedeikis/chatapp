3d303aa41cd6547284c8e0a98e3724163a413f13
Sarunas Medeikis <SarunasMedeikis@users.noreply.github.com>
2021-02-08 16:28:01 +0200

    add message to DB

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React from "react";
+import * as FirestoreService from "./services/firestore";
 
 function InitialScreen({ userName, setUserName }) {
   function onChange(event) {
@@ -25,6 +26,71 @@ function InitialScreen({ userName, setUserName }) {
   );
 }
 
+function ChatBox({ userName }) {
+  const [message, setMessage] = React.useState("");
+
+  function onChange(event) {
+    return setMessage(event.target.value);
+  }
+  function sendMessage(event) {
+    event.preventDefault();
+    FirestoreService.addMessage(userName["userName"], message)
+      .then((e) => {
+        console.log(e);
+        setMessage("");
+      })
+      .catch((e) => console.error(e));
+  }
+  return (
+    <div className="containerChatBox">
+      <div className="usersInChat">
+        <h5>Currently in chat</h5>
+        <ul>
+          <li>John Doe</li>
+          <li>John Doe</li>
+          <li>John Doe</li>
+          <li>John Doe</li>
+        </ul>
+      </div>
+      <div className="chatBox">
+        <div className="forScrolling">
+          <div className="messageStyles">
+            <span className="messageSentUserName">John Doe</span>
+            <p>
+              Lorem ipsum color sit it us Lorem ipsum color sit it us Lorem
+              ipsum color sit it us Lorem ipsum color sit it us Lorem ipsum
+              color sit it us Lorem ipsum color sit it usLorem ipsum color sit
+              it us Lorem ipsum color sit it us Lorem ipsum color sit it us
+            </p>
+            <span className="messageSentTime">13:45</span>
+          </div>
+          <div className="messageStyles">
+            <span className="messageSentUserName">John Doe</span>
+            <p>
+              Lorem ipsum color sit it us Lorem ipsum color sit it us Lorem
+              ipsum color sit it us Lorem ipsum color sit it us Lorem ipsum
+              color sit it us Lorem ipsum color sit it usLorem ipsum color sit
+              it us Lorem ipsum color sit it us Lorem ipsum color sit it us
+            </p>
+            <span className="messageSentTime">13:45</span>
+          </div>
+        </div>
+        <div className="inputMessageField">
+          <form onSubmit={sendMessage}>
+            <input
+              type="text"
+              placeholder="Your message"
+              value={message}
+              onChange={onChange}
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   const [userName, setUserName] = React.useState({
     hasUsername: false,
@@ -35,7 +101,10 @@ function App() {
     <div className="App">
       <div className="App-header">
         {userName.hasUsername ? (
-          <h1>userName = {userName.userName}</h1>
+          <>
+            <h1>userName = {userName.userName}</h1>
+            <ChatBox userName={userName} />
+          </>
         ) : (
           <InitialScreen userName={userName} setUserName={setUserName} />
         )}
--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,6 +1,4 @@
-import * as firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,3 +9,24 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 console.log(db);
+
+export const authenticateAnonymously = () => {
+  return firebase.auth().signInAnonymously();
+};
+
+export const addMessage = (userName, message) => {
+  let today = Date.now().toString();
+  // Add a new message to "messages" collection
+  return db
+    .collection("messages")
+    .doc(`${today}${userName}`)
+    .set({
+      message: message,
+    })
+    .then(() => {
+      console.log("Document successfully written!");
+    })
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    });
+};
